93786bccd9a50a8b32664c54c5a803ab3390c6f8
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2025-03-03 23:10:30 -0300

    chore(nvim): implement new lsp languages

--- .config/nvim/lua/davyd-souza/plugins/lsp.lua
+++ .config/nvim/lua/davyd-souza/plugins/lsp.lua
@@ -114,11 +114,15 @@ return {
 			vim.list_extend(ensure_installed, {
 				"stylua",
 				"biome",
+				"typescript-language-server",
+				"tailwindcss-language-server",
 			})
 
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				automatic_installation = false,
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
