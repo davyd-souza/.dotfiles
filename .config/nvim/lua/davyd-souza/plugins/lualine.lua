7a34701498406f0355495acb392974ebabaa1c76
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2025-08-23 02:56:03 -0300

    style(nvim): format lualine file

--- .config/nvim/lua/davyd-souza/plugins/lualine.lua
+++ .config/nvim/lua/davyd-souza/plugins/lualine.lua
@@ -1,16 +1,18 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require('lualine').setup({
+		require("lualine").setup({
 			options = {
-				theme = 'catppuccin',
-				section_separators = '',
-				component_separators = '',
+				theme = "catppuccin",
+				section_separators = "",
+				component_separators = "",
 			},
 			sections = {
-				lualine_a = {'mode'},
+				lualine_a = { "mode" },
 				lualine_b = {
+					"filename",
+					--[[
 					{
 						'buffers',
 						show_filename_only = true,
@@ -19,37 +21,38 @@ return {
 
 						mode = 0,
 					},
+					--]]
 				},
-				lualine_c = {'diff'},
+				lualine_c = { "diff" },
 
 				lualine_x = {
 					{
-						'diagnostics',
+						"diagnostics",
 
-						sources = { 'nvim_lsp' },
+						sources = { "nvim_lsp" },
 
-						sections = { 'error', 'warn', 'info', 'hint' },
+						sections = { "error", "warn", "info", "hint" },
 						diagostics_color = {
-							error = 'DiagnosticError',
-							warn = 'DiagnosticWarn',
-							info = 'DiagnosticInfo',
-							hint = 'DiagnosticHint',
+							error = "DiagnosticError",
+							warn = "DiagnosticWarn",
+							info = "DiagnosticInfo",
+							hint = "DiagnosticHint",
 						},
 
 						symbols = {
-							error = 'E',
-							warn = 'W',
-							info = 'I',
-							hint = 'H'
+							error = "E",
+							warn = "W",
+							info = "I",
+							hint = "H",
 						},
 
 						colored = true,
 						update_in_insert = false,
 					},
 				},
-				lualine_y = {'encoding', 'filetype'},
-				lualine_z = {'branch'},
-			}
+				lualine_y = { "encoding", "filetype" },
+				lualine_z = { "branch" },
+			},
 		})
 	end,
 }
