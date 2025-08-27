8d4e0e4325298d9b4f69614dcfe1ace1f2a14cfe
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2025-08-27 04:42:00 -0300

    refactor(nvim): update conform

--- .config/nvim/lua/davyd-souza/plugins/formatter.lua
+++ .config/nvim/lua/davyd-souza/plugins/formatter.lua
@@ -6,23 +6,24 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 
-				javascript = { "prettier", "biome", stop_after_first = true },
-				typescript = { "prettier", "biome", stop_after_first = true },
-				javascriptreact = { "prettier", "biome", stop_after_first = true },
-				typescriptreact = { "prettier", "biome", stop_after_first = true },
+				javascript = { "prettier", "biome" },
+				typescript = { "prettier", "biome" },
+				javascriptreact = { "prettier", "biome" },
+				typescriptreact = { "prettier", "biome" },
 
-				-- astro = { "biome", "prettier", stop_after_first = true },
+				astro = { "biome", "prettier", stop_after_first = true },
 			},
 
 			format_on_save = {
 				timeout_ms = 500,
-				lsp_format = "fallback",
+				lsp_format = "never",
 				-- async = true,
 			},
 		})
 
 		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = { ".lua" },
+			-- pattern = { ".lua" },
+			pattern = "*",
 			callback = function(args)
 				--[[
 				local ignore_ft = { "%.tsx?$", "%.jsx?$" }
